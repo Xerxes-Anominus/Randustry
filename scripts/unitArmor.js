d76d817f449c4e2939aa0dee99ba367bb06b097c
Xerxes <xerxesanominus@gmail.com>
2026-06-16 10:01:49 +0700

    Create unitArmor.js

---
+++ scripts/unitArmor.js
@@ -0,0 +1,49 @@
+function armor(a) {
+    return (Math.ceil(a * (Math.random() * 2)));
+}
+
+UnitTypes.alpha.armor = armor(UnitTypes.alpha.armor);
+UnitTypes.beta.armor = armor(UnitTypes.beta.armor);
+UnitTypes.gamma.armor = armor(UnitTypes.gamma.armor);
+
+UnitTypes.dagger.armor = armor(UnitTypes.dagger.armor);
+UnitTypes.mace.armor = armor(UnitTypes.mace.armor);
+UnitTypes.fortress.armor = armor(UnitTypes.fortress.armor);
+UnitTypes.scepter.armor = armor(UnitTypes.scepter.armor);
+UnitTypes.reign.armor = armor(UnitTypes.reign.armor);
+
+UnitTypes.crawler.armor = armor(UnitTypes.crawler.armor);
+UnitTypes.atrax.armor = armor(UnitTypes.atrax.armor);
+UnitTypes.spiroct.armor = armor(UnitTypes.spiroct.armor);
+UnitTypes.arkyid.armor = armor(UnitTypes.arkyid.armor);
+UnitTypes.toxopid.armor = armor(UnitTypes.toxopid.armor);
+
+UnitTypes.nova.armor = armor(UnitTypes.nova.armor);
+UnitTypes.pulsar.armor = armor(UnitTypes.pulsar.armor);
+UnitTypes.quasar.armor = armor(UnitTypes.quasar.armor);
+UnitTypes.vela.armor = armor(UnitTypes.vela.armor);
+UnitTypes.corvus.armor = armor(UnitTypes.corvus.armor);
+
+UnitTypes.flare.armor = armor(UnitTypes.flare.armor);
+UnitTypes.horizon.armor = armor(UnitTypes.horizon.armor);
+UnitTypes.zenith.armor = armor(UnitTypes.zenith.armor);
+UnitTypes.antumbra.armor = armor(UnitTypes.antumbra.armor);
+UnitTypes.eclipse.armor = armor(UnitTypes.eclipse.armor);
+
+UnitTypes.mono.armor = armor(UnitTypes.mono.armor);
+UnitTypes.poly.armor = armor(UnitTypes.poly.armor);
+UnitTypes.mega.armor = armor(UnitTypes.mega.armor);
+UnitTypes.quad.armor = armor(UnitTypes.quad.armor);
+UnitTypes.oct.armor = armor(UnitTypes.oct.armor);
+
+UnitTypes.risso.armor = armor(UnitTypes.risso.armor);
+UnitTypes.minke.armor = armor(UnitTypes.minke.armor);
+UnitTypes.bryde.armor = armor(UnitTypes.bryde.armor);
+UnitTypes.sei.armor = armor(UnitTypes.sei.armor);
+UnitTypes.omura.armor = armor(UnitTypes.omura.armor);
+
+UnitTypes.retusa.armor = armor(UnitTypes.retusa.armor);
+UnitTypes.oxynoe.armor = armor(UnitTypes.oxynoe.armor);
+UnitTypes.cyerce.armor = armor(UnitTypes.cyerce.armor);
+UnitTypes.aegires.armor = armor(UnitTypes.aegires.armor);
+UnitTypes.navanax.armor = armor(UnitTypes.navanax.armor);
