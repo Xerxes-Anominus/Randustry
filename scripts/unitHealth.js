5ec9f918a3678c22ab1002b612eaebdb713a423a
Xerxes <xerxesanominus@gmail.com>
2026-06-16 10:00:49 +0700

    Create unitHealth.js

---
+++ scripts/unitHealth.js
@@ -0,0 +1,49 @@
+function health(a) {
+    return (Math.ceil(a * (Math.random()*2)));
+}
+
+UnitTypes.alpha.health = health(UnitTypes.alpha.health);
+UnitTypes.beta.health = health(UnitTypes.beta.health);
+UnitTypes.gamma.health = health(UnitTypes.gamma.health);
+
+UnitTypes.dagger.health = health(UnitTypes.dagger.health);
+UnitTypes.mace.health = health(UnitTypes.mace.health);
+UnitTypes.fortress.health = health(UnitTypes.fortress.health);
+UnitTypes.scepter.health = health(UnitTypes.scepter.health);
+UnitTypes.reign.health = health(UnitTypes.reign.health);
+
+UnitTypes.crawler.health = health(UnitTypes.crawler.health);
+UnitTypes.atrax.health = health(UnitTypes.atrax.health);
+UnitTypes.spiroct.health = health(UnitTypes.spiroct.health);
+UnitTypes.arkyid.health = health(UnitTypes.arkyid.health);
+UnitTypes.toxopid.health = health(UnitTypes.toxopid.health);
+
+UnitTypes.nova.health = health(UnitTypes.nova.health);
+UnitTypes.pulsar.health = health(UnitTypes.pulsar.health);
+UnitTypes.quasar.health = health(UnitTypes.quasar.health);
+UnitTypes.vela.health = health(UnitTypes.vela.health);
+UnitTypes.corvus.health = health(UnitTypes.corvus.health);
+
+UnitTypes.flare.health = health(UnitTypes.flare.health);
+UnitTypes.horizon.health = health(UnitTypes.horizon.health);
+UnitTypes.zenith.health = health(UnitTypes.zenith.health);
+UnitTypes.antumbra.health = health(UnitTypes.antumbra.health);
+UnitTypes.eclipse.health = health(UnitTypes.eclipse.health);
+
+UnitTypes.mono.health = health(UnitTypes.mono.health);
+UnitTypes.poly.health = health(UnitTypes.poly.health);
+UnitTypes.mega.health = health(UnitTypes.mega.health);
+UnitTypes.quad.health = health(UnitTypes.quad.health);
+UnitTypes.oct.health = health(UnitTypes.oct.health);
+
+UnitTypes.risso.health = health(UnitTypes.risso.health);
+UnitTypes.minke.health = health(UnitTypes.minke.health);
+UnitTypes.bryde.health = health(UnitTypes.bryde.health);
+UnitTypes.sei.health = health(UnitTypes.sei.health);
+UnitTypes.omura.health = health(UnitTypes.omura.health);
+
+UnitTypes.retusa.health = health(UnitTypes.retusa.health);
+UnitTypes.oxynoe.health = health(UnitTypes.oxynoe.health);
+UnitTypes.cyerce.health = health(UnitTypes.cyerce.health);
+UnitTypes.aegires.health = health(UnitTypes.aegires.health);
+UnitTypes.navanax.health = health(UnitTypes.navanax.health);
